21845f5d6f0f7fc3b59fa64b7f715017d3878147
Beau Elliott <belliott1516@gmail.com>
2022-07-12 10:28:16 -0700

    added titlecase test

--- index.test.js
+++ index.test.js
@@ -5,3 +5,9 @@ describe("reverseWords", () => {
     expect(reverseWords("alchemy rocks gold")).toBe("ymehcla skcor dlog");
   });
 });
+
+describe("titleCase", () => {
+  it("should change each word to be first letter capitalized and rest lowercase", () => {
+    expect(reverseWords("alchemy ROCKS goLD")).toBe("Alchemy Rocks Gold");
+  });
+});
