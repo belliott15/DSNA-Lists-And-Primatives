d7f5f4f3ac3f77fe5e5ee448438db1dab3a1af1d
JeffAllison <itsjallison@gmail.com>
2022-07-12 10:25:00 -0700

    reverse letter test and function passing

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function reverseWords(sentence) {}
+function reverseWords(sentence) {
+  // //reversing letters, word order remains
+  // const word = sentence.split(' ');
+  // // split the words indepentently
+
+  // const reverse = word.split('');
+  // //join words in reverse
+  // console.log(reverse);
+
+  const word = sentence
+    .split('')
+    .reverse()
+    .join('')
+    .split(' ')
+    .reverse()
+    .join(' ');
+
+  console.log(word);
+  return word;
+}
 
 module.exports = { reverseWords };
